df538758b993f6f5fa6271f8b1391ff407c509cb
Dressel107 <88779152+Dressel107@users.noreply.github.com>
2022-02-23 19:18:31 +0100

    ansätze languange

--- app/src/main/java/com/example/pushyapp/Activities/LevelSelectionActivity.java
+++ app/src/main/java/com/example/pushyapp/Activities/LevelSelectionActivity.java
@@ -3,30 +3,39 @@ package com.example.pushyapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.pushyapp.ListAdapters.LevelListItemAdapter;
 import com.example.pushyapp.Models.Level;
 import com.example.pushyapp.Models.LevelPool;
 import com.example.pushyapp.R;
+import com.example.pushyapp.Services.LocaleHelper;
 
 import java.util.ArrayList;
 
 public class LevelSelectionActivity extends AppCompatActivity
 {
     private ListView list;
+    Context context;
+    Resources resources;
+    TextView levelSecound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_selection);
+
+        levelSecound = findViewById(R.id.level_needed_time);
 
         LevelListItemAdapter adapter = new LevelListItemAdapter(this, LevelPool.getAll());
         list=(ListView)findViewById(R.id.level_selection_listView);
@@ -55,10 +64,14 @@ public class LevelSelectionActivity extends AppCompatActivity
         switch (item.getItemId()) {
 
             case R.id.german:
-
+                context = LocaleHelper.setLocale(LevelSelectionActivity.this, "en");
+                resources = context.getResources();
+                levelSecound.setText(resources.getString(R.string.secounds));
                 System.out.println("german");
             case R.id.english:
-
+                context = LocaleHelper.setLocale(LevelSelectionActivity.this, "de");
+                resources = context.getResources();
+                levelSecound.setText(resources.getString(R.string.secounds));
                 System.out.println("english");
 
                 return true;

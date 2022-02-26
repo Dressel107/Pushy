581452b6042086a693241446f1d8e59d7554e064
Dressel107 <88779152+Dressel107@users.noreply.github.com>
2022-02-26 12:27:08 +0100

    UnitTest für ColorSphere

---
+++ app/src/test/java/com/example/pushyapp/Models/GameElements/ColorSphereTest.java
@@ -0,0 +1,40 @@
+package com.example.pushyapp.Models.GameElements;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.example.pushyapp.Enums.Color;
+import com.example.pushyapp.Enums.Direction;
+
+/**
+ * @author Dirk Dresselhaus
+ */
+public class ColorSphereTest extends TestCase {
+
+    @Test
+    public void testReset() {
+        ColorSphere cs = new ColorSphere(5,5, Color.Blue);
+        cs.move(Direction.Left);
+        cs.setColor(Color.Red);
+        cs.reset();
+        assertEquals(Color.Blue, cs.getColor());
+        assertEquals(5,5);
+    }
+
+    @Test
+    public void testMove() {
+        ColorSphere cs = new ColorSphere(5,5, Color.Blue);
+        cs.move(Direction.Left);
+        assertEquals(4, cs.getX());
+    }
+
+    @Test
+    public void testTryInteract() {
+        ColorSphere colorSphere = new ColorSphere(5,5, Color.Blue);
+        ColorSplash colorSplash = new ColorSplash(3,3 ,Color.Red);
+        colorSphere.tryInteract(colorSplash);
+        assertEquals(Color.Red, colorSphere.getColor());
+
+    }
+}
